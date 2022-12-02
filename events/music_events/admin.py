c9ed6c1a629752d9dc0d80eadd2e6689ea7b4bac
Fernando Molano <53359507+fermolanoc@users.noreply.github.com>
2022-12-01 20:27:33 -0500

    [ADD] models, register models on admin file to access them through admin panel

--- events/music_events/admin.py
+++ events/music_events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Venue, Artist, Show
+
+admin.site.register(Venue)
+admin.site.register(Artist)
+admin.site.register(Show)
--- events/music_events/models.py
+++ events/music_events/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+class Artist(models.Model):
+    """ Represents a musician or a band - a music artist """
+    name = models.CharField(max_length=200, blank=False)
+
+    def __str__(self):
+        return f'Name: {self.name}'
+
+
+class Venue(models.Model):
+    """ Represents a venue, that hosts shows. """
+    name = models.CharField(max_length=200, blank=False, unique=True)
+    city = models.CharField(max_length=200, blank=False)
+    state = models.CharField(max_length=2, blank=False)
+
+    def __str__(self):
+        return f'Name: {self.name} Location: {self.city}, {self.state}'
+
+
+class Show(models.Model):
+    """ One Artist playing at one Venue at a particular date and time. """
+    show_date = models.DateTimeField(blank=False)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Artist: {self.artist} At: {self.venue} On: {self.show_date}'
